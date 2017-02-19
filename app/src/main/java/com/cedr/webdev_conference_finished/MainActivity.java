fc444aa756b7e0aebf33d66632a76ce0b8fef7f4
jraco <jraco11@gmail.com>
2017-02-19 14:35:33 -0800

    initial commit

---
+++ app/src/main/java/com/cedr/webdev_conference_finished/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cedr.webdev_conference_finished;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
